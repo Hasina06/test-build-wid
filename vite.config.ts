eed43a86c0719c033b8f00cc03df9fa0b41b4dcf
Hasina <herihasinamamues@gmeil.com>
2025-04-06 15:52:37 +0200

    config base: vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/client-build-wid/',
+  base: '/test-build-wid/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
